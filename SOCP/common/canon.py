d216adc85a7580b60629eeaa21b5badb37109997
timothieecantcode <timothienguyen@gmail.com>
2025-10-05 17:32:02 +1030

    helper for JSON data hashing

---
+++ SOCP/common/canon.py
@@ -0,0 +1,16 @@
+import json
+import hashlib
+
+
+# ---- Convert data to JSON format and compute its SHA-256 hash for signing or verification ---- #
+def canon(obj) -> bytes:
+    # return (sorted key, no whitespace) JSON-encoded bytes
+    return json.dumps(obj, sort_keys=True, separator=(',', ':')).encode('utf-8')
+
+
+def sha256_bytes(*chunks: bytes) -> bytes:
+    # compute SHA-256 hassh of concatenated byte chunks and return raw 32 bytes digest
+    h = hashlib.sha256()
+    for c in chunks:
+        h.update(c)
+    return h.digest()
